02b6822c07103afe6c64bba158570cfe21c19674
Jenny Yang <michel3153@gmail.com>
2025-11-02 14:52:31 +0000

    add missing output

--- terraform/modules/database/outputs.tf
+++ terraform/modules/database/outputs.tf
@@ -1,3 +1,7 @@
 output "db_connection_url" {
   value = "postgresql://${var.db_user}:${var.db_password}@${google_sql_database_instance.postgres.ip_address[0].ip_address}/${var.db_name}"
 }
+
+output "db_private_ip" {
+  value = google_sql_database_instance.postgres.ip_address[0].ip_address
+}
