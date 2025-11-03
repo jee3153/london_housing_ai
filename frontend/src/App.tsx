5f29008747338a16d926f0c43a30c00219df3e4a
Jenny Yang <michel3153@gmail.com>
2025-11-03 16:14:05 +0000

    initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+import './App.css'
+import './components/ModelPerformance'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      </div >
+    </>
+  )
+}
+
+export default App
