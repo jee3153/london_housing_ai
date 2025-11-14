f425973f85b127fd80b2336d04246ed3bb2fe179
Jenny Yang <michel3153@gmail.com>
2025-11-14 10:42:55 +0000

    uncomment artifact upload logic

--- backend/src/london_housing_ai/train_main.py
+++ backend/src/london_housing_ai/train_main.py
@@ -86,20 +86,20 @@ def main(args: Namespace) -> None:
 
         # ------comment it only when gcs uploading is required.
         # silver layer check-point
-        # parquet_config = load_parquet_config(config_path)
-        # parquet_dir = data_path / "silver"
+        parquet_config = load_parquet_config(config_path)
+        parquet_dir = data_path / "silver"
 
-        # write_df_to_partitioned_parquet(
-        #     df=df,
-        #     out_dir=parquet_dir,
-        #     partition_cols=parquet_config.silver_partition_cols,
-        # )
-        # upload_parquet_to_gcs(
-        #     local_dir=parquet_dir,
-        #     destination_blob_name=parquet_config.destination_blob_name,
-        #     credential_path=os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"),
-        #     cleanup=args.cleanup_local,
-        # )
+        write_df_to_partitioned_parquet(
+            df=df,
+            out_dir=parquet_dir,
+            partition_cols=parquet_config.silver_partition_cols,
+        )
+        upload_parquet_to_gcs(
+            local_dir=parquet_dir,
+            destination_blob_name=parquet_config.destination_blob_name,
+            credential_path=os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"),
+            cleanup=args.cleanup_local,
+        )
         # -------end of gcs uploading
 
         df = asyncio.run(
